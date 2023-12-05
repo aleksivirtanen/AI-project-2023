486f80737117cbadac2e0c6f82be5abfdbbc24be
Aleksi Virtanen <avek91@hotmail.com>
2023-12-05 18:34:26 +0200

    Added OpenAIApi fetch function

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,38 +3,69 @@ import Input from "../components/Input";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const MainPage = () => {
+  const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+
   const inputRef = useRef();
   const [inputs, setInputs] = useState([]);
+  const [responses, setResponses] = useState([]);
   const [counter, setCounter] = useState(-1);
   const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
   const clickHandler = (event) => {
     event.preventDefault();
+    setDisabled(true);
     setInputs([...inputs, inputRef.current.value]);
     setCounter(counter + 1);
     postQuestion(inputRef.current.value);
     inputRef.current.value = "";
+    setTimeout(() => {
+      setDisabled(false);
+    }, 10000);
   };
 
   const postQuestion = async (question) => {
-    console.log(question);
-    setLoading(true); /*
-    const response = await fetch("asdasd", {
-      method: "POST",
-      body: JSON.stringify(question),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);*/
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+          },
+          body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content: "Your answers are short and straight to the point.",
+              },
+              { role: "user", content: question },
+            ],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Oops! Something went wrong.");
+      }
+
+      const responseData = await response.json();
+
+      setResponses([...responses, responseData.choices[0].message.content]);
+      console.log(responseData.choices[0]);
+    } catch (error) {
+      console.error("Error while fetching chat data:", error);
+    }
     setLoading(false);
   };
 
   return (
     <div>
       <Input id="input" ref={inputRef} />
-      <button type="button" onClick={clickHandler}>
+      <button type="button" disabled={disabled} onClick={clickHandler}>
         Send
       </button>
       {loading && (
@@ -42,7 +73,8 @@ const MainPage = () => {
           <LoadingSpinner />
         </div>
       )}
-      <div>{inputs[counter]}</div>
+      <div>INPUT: {inputs[counter]}</div>
+      <div>RESPONSE: {responses[counter]}</div>
     </div>
   );
 };
